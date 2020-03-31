369a39fbb00886ffd75bcbb441b8778f527282c6
Noman Shoukat <nomanaadma@gmail.com>
2020-03-31 21:57:26 +0500

    Clean up MVC

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,13 @@
 <?php
   // DB Params
   define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASS', '');
-  define('DB_NAME', 'MVCpractice');
+  define('DB_USER', '_YOUR_USER_');
+  define('DB_PASS', '_YOUR_PASS_');
+  define('DB_NAME', '_YOUR_DBNAME_');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://core-mvc-framework-concepts-practice.test');
+  define('URLROOT', '_YOUR_URL_');
   // Site Name
-  define('SITENAME', 'MVCpractice');
+  define('SITENAME', '_YOUR_SITENAME_');
